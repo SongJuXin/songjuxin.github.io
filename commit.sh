48e22d43f9a4b9ed7d375e6343c20913f6b70999
Simba <1016346103@qq.com>
2025-03-30 18:46:01 +0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -7,8 +7,9 @@ if [ $# -ne 2 ]; then
 fi
 
 # 从参数中获取提交信息和文件名
-commit_message=$1
+data=$1
 file_name=$2
+file_path="submissions/${file_name}.txt"
 
 # 最大重试次数
 max_retries=5
@@ -19,9 +20,9 @@ while [ $retry_count -lt $max_retries ]; do
   git fetch origin
   git merge origin/$current_branch --no-edit
   if [ $? -eq 0 ]; then
-    echo "$commit_message" >> "$file_name"
-    git add "$file_name"
-    git commit -m "$commit_message"
+    echo "$data" >> "$file_path"
+    git add "$file_path"
+    git commit -m "update$file_name"
     git push
     if [ $? -eq 0 ]; then
       echo "Push successful"
